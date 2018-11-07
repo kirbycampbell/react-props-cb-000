f201dc076ecc4c4efed582c7426304978889042c
Kirby Campbell <jkirbycampbell@gmail.com>
2018-11-07 20:08:15 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -36,7 +36,6 @@ const jurassicWorldGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller
 ReactDOM.render(
   <MovieCard
     title="Jurassic World"
-    poster="http://image.tmdb.org/t/p/w342/jjBgi2r5cRt36xF6iNUEhzscEcb.jpg"
     genres={jurassicWorldGenres}
   />,
   document.getElementById('root')
